2251639af10da8f1b1eb0513aad6fdc21dc9b6d4
arnabghorui <61308067+arnabghorui@users.noreply.github.com>
2020-03-12 20:36:35 +0530

    Update qr_eigen.py

--- qr_eigen.py
+++ qr_eigen.py
@@ -3,14 +3,15 @@ np.set_printoptions(precision=3)
 np.set_printoptions(suppress=True)
 
 A = np.array([[5, -2],
-			  [-2, 8]])
+              [-2, 8]])
 A1 = A
-for i in range(1000):
-	q, r  = np.linalg.qr(A1)
-	A1 = r.dot(q)
-
-# print(A1)
-print("Eigenvalues calculated from QR decomposition:", "%.2f" %A1[0][0],"and",  "%.2f" %A1[1][1])
+count = 0
+while True:
+    q, r  = np.linalg.qr(A1)
+    A1 = r.dot(q)
+    if(abs(A1[0][1]) < 10**-10 and abs(A1[1][0]) < 10**-10):
+        break
+print("Eigenvalues calculated from QR decomposition:", "%.2f" %A1[1][1],"and",  "%.2f" %A1[0][0])
 
 eig_val, eig_vec = np.linalg.eigh(A)
 print("Eigenvalues calculated from 'np.linalg.eigh':", eig_val)
